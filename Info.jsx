4e6369f959c0ba7c11d4d0b00abb4cc8c2d95bb8
AmmaarullahHussain <85430380+AmmaarullahHussain@users.noreply.github.com>
2021-06-28 23:01:33 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+      <h2>Basic WebDev using Javascript and React.JS</h2>
+      <p>This was an amazing bootcamp conducted Shaurya Sinha sir</p>
+    </div>
+  );
+}
+
+export default Info;
